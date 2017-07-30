02e55d03194435895ad97feb8331a9ce7289506f
Hajime Hoshi <hajimehoshi@gmail.com>
2017-07-31 02:10:57 +0900

    Refine arc

--- shapes/main.go
+++ shapes/main.go
@@ -43,6 +43,14 @@ func (l *Line) Intersect(y float64) ([]float64, bool) {
 	return []float64{x}, true
 }
 
+func (l *Line) Start() PointF {
+	return l.P0
+}
+
+func (l *Line) End() PointF {
+	return l.P1
+}
+
 type Arc struct {
 	X          float64
 	Y          float64
@@ -52,27 +60,74 @@ type Arc struct {
 	//AntiClockwise bool
 }
 
+func normalizeAngle(a float64, start, end float64) float64 {
+	for a < start {
+		a += 2 * math.Pi
+	}
+	for end <= a {
+		a -= 2 * math.Pi
+	}
+	return a
+}
+
 func (a *Arc) Intersect(y float64) ([]float64, bool) {
 	if a.Radius <= 0 {
 		return nil, true
 	}
-	if a.Y - a.Radius > y {
+	if a.Y-a.Radius > y {
 		return nil, true
 	}
-	if a.Y + a.Radius < y {
+	if a.Y+a.Radius < y {
 		return nil, true
 	}
 	t := math.Pow(a.Radius, 2) - math.Pow(y-a.Y, 2)
-	x0 := math.Sqrt(t) + a.X
-	x1 := -math.Sqrt(t) + a.X
+	x0 := -math.Sqrt(t) + a.X
+	x1 := math.Sqrt(t) + a.X
 	if x0 == x1 {
 		return nil, false
 	}
-	return []float64{x0, x1}, true
+	as := a.StartAngle
+	ae := a.EndAngle
+	a0 := normalizeAngle(math.Asin((y - a.Y) / a.Radius), 0, 2 * math.Pi)
+	a1 := normalizeAngle(-a0 + math.Pi, 0, 2 * math.Pi)
+	if a0 < math.Pi/2 || 3*math.Pi/2 <= a0 {
+		tmp := a0
+		a0 = a1
+		a1 = tmp
+	}
+	a0 = normalizeAngle(a0, as, ae)
+	a1 = normalizeAngle(a1, as, ae)
+	r := []float64{}
+	if a0 == as || a0 == ae || a1 == as || a1 == ae {
+		return nil, false
+	}
+	if as < a0 && a0 < ae {
+		r = append(r, x0)
+	}
+	if as < a1 && a1 < ae {
+		r = append(r, x1)
+	}
+	return r, true
+}
+
+func (a *Arc) Start() PointF {
+	return PointF{
+		X: a.Radius*math.Cos(a.StartAngle) + a.X,
+		Y: a.Radius*math.Sin(a.StartAngle) + a.Y,
+	}
+}
+
+func (a *Arc) End() PointF {
+	return PointF{
+		X: a.Radius*math.Cos(a.EndAngle) + a.X,
+		Y: a.Radius*math.Sin(a.EndAngle) + a.Y,
+	}
 }
 
 type Intersecter interface {
 	Intersect(y float64) ([]float64, bool)
+	Start() PointF
+	End() PointF
 }
 
 type Path struct {
@@ -81,8 +136,12 @@ type Path struct {
 }
 
 func (p Path) Intersect(y float64) ([]float64, bool) {
+	is := p.intersecters
+	if len(is) == 0 {
+		return nil, true
+	}
 	r := []float64{}
-	for _, i := range p.intersecters {
+	for _, i := range is {
 		xs, ok := i.Intersect(y)
 		if !ok {
 			return nil, ok
@@ -138,13 +197,13 @@ func colorAt(path *Path, x, y int) uint8 {
 	return uint8(color * 255)
 }
 
-func (p *Path) moveTo(point PointF) {
-	p.pos = point
+func (p *Path) moveTo(x, y float64) {
+	p.pos = PointF{x, y}
 }
 
-func (p *Path) lineTo(point PointF) {
-	p.intersecters = append(p.intersecters, &Line{p.pos, point})
-	p.pos = point
+func (p *Path) lineTo(x, y float64) {
+	p.intersecters = append(p.intersecters, &Line{p.pos, PointF{x, y}})
+	p.pos = PointF{x, y}
 }
 
 func (p *Path) rect(x, y, length float64) {
@@ -158,16 +217,23 @@ func (p *Path) rect(x, y, length float64) {
 		&Line{p1, p2},
 		&Line{p2, p3},
 		&Line{p3, p0})
+	p.pos = p0
 }
 
-func (p *Path) arc(x, y, radius float64) {
-	p.intersecters = append(
-		p.intersecters,
-		&Arc{
-			X:      x,
-			Y:      y,
-			Radius: radius,
-		})
+func (p *Path) arc(x, y, radius, startAngle, endAngle float64) {
+	arc := &Arc{
+		X:          x,
+		Y:          y,
+		Radius:     radius,
+		StartAngle: startAngle,
+		EndAngle:   endAngle,
+	}
+	if p.pos != arc.Start() {
+		line := &Line{p.pos, arc.Start()}
+		p.intersecters = append(p.intersecters, line)
+	}
+	p.intersecters = append(p.intersecters, arc)
+	p.pos = arc.End()
 }
 
 var count = 0
@@ -178,14 +244,16 @@ func update(screen *ebiten.Image) error {
 	p1 := PointF{20, 30}
 	p2 := PointF{40, 35}
 	p3 := PointF{30, 25}
-	path.moveTo(p0)
+	path.moveTo(p0.X, p0.Y)
 	for _, p := range []PointF{p0, p1, p2, p3, p0} {
-		path.lineTo(p)
+		path.lineTo(p.X, p.Y)
 	}
 	path.rect(130, 30, 100)
 	path.rect(130.5, 40+float64(count)/15.0, 100)
 
-	path.arc(50, 50, 30)
+	path.moveTo(50, 50)
+	path.arc(50, 50, 30, math.Pi / 4, 2 * math.Pi + math.Pi / 8)
+	path.lineTo(50, 50)
 
 	for j := 0; j < screenHeight; j++ {
 		for i := 0; i < screenWidth; i++ {
